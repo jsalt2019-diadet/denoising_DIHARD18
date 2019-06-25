372050da8a34f1feb86d307fbf4f8a6f91b5a22f
staplesinLA <307497253@qq.com>
2019-06-25 18:01:09 -0400

    Update run_eval.sh

--- run_eval.sh
+++ run_eval.sh
@@ -6,30 +6,18 @@
 ###################################
 # Run speech enhancement
 ###################################
-WAV_DIR=/data/wav/  # Directory of WAV files (16 kHz, 16 bit) to enhance.
-SE_WAV_DIR=/data/wav_pn_enhanced  # Output directory for enhanced WAV.
+WAV_DIR=/export/fs01/jsalt19/leisun/speech_enhancement/denoising_DIHARD18/testing_files  # Directory of WAV files (16 kHz, 16 bit) to enhance.
+SE_WAV_DIR=/export/fs01/jsalt19/leisun/speech_enhancement/denoising_DIHARD18/testing_files_enhanced_lps  # Output directory for enhanced WAV.
 USE_GPU=true  # Use GPU instead of CPU. To instead use CPU, set to 'false'.
 GPU_DEVICE_ID=0  # Use GPU with device id 0. Irrelevant if using CPU.
 TRUNCATE_MINUTES=10  # Duration in minutes of chunks for enhancement. If you experience
                      # OOM errors with your GPU, try reducing this.
+MODE=1   # Use which estimated output of the model: mode=1 is irm(conservative), mode=2 is lps(agressive), mode=3(fusion) is fusion of both.
 python main_denoising.py \
        --verbose \
        --wav_dir $WAV_DIR --output_dir $SE_WAV_DIR \
        --use_gpu $USE_GPU --gpu_id $GPU_DEVICE_ID \
-       --truncate_minutes $TRUNCATE_MINUTES || exit 1
+       --truncate_minutes $TRUNCATE_MINUTES \
+       --mode $MODE|| exit 1
 
-
-###################################
-# Perform VAD using enhanced audio
-###################################
-VAD_DIR=/data/vad  # Output directory for label files containing VAD output.
-HOPLENGTH=30  # Duration in milliseconds of frames for VAD. Also controls step size.
-MODE=3  # WebRTC aggressiveness. 0=least agressive and  3=most aggresive.
-NJOBS=1  # Number of parallel processes to use.
-python main_get_vad.py \
-       --verbose \
-       --wav_dir $SE_WAV_DIR --output_dir $VAD_DIR \
-       --mode $MODE --hoplength $HOPLENGTH \
-       --n_jobs $NJOBS || exit 1
-
-exit 0
+exit
