b2ec4a89a7e213b6f5fabd44a5d99d7ea43095e6
staplesinLA <307497253@qq.com>
2019-06-28 17:22:20 -0400

    Update run_eval.sh

--- run_eval.sh
+++ run_eval.sh
@@ -6,18 +6,47 @@
 ###################################
 # Run speech enhancement
 ###################################
-WAV_DIR=/export/fs01/jsalt19/leisun/speech_enhancement/denoising_DIHARD18/testing_files  # Directory of WAV files (16 kHz, 16 bit) to enhance.
-SE_WAV_DIR=/export/fs01/jsalt19/leisun/speech_enhancement/denoising_DIHARD18/testing_files_enhanced_lps  # Output directory for enhanced WAV.
+export PATH=/home/leisun1/local/mpi/bin:$PATH
+export LD_LIBRARY_PATH=/home/leisun1/local/mpi/lib:$LD_LIBRARY_PATH
+
+
+WAV_DIR=/export/fs01/jsalt19/leisun/dataset/SRI/testset/origin_uncomplete/  # Directory of WAV files (16 kHz, 16 bit) to enhance.
+SE_WAV_DIR=/export/fs01/jsalt19/leisun/dataset/SRI/testset/SE_1000h_model_m3_s3/
+# Output directory for enhanced WAV.
 USE_GPU=true  # Use GPU instead of CPU. To instead use CPU, set to 'false'.
-GPU_DEVICE_ID=0  # Use GPU with device id 0. Irrelevant if using CPU.
+GPU_DEVICE_ID=`free-gpu`  # Use GPU with device id 0. Irrelevant if using CPU.
 TRUNCATE_MINUTES=10  # Duration in minutes of chunks for enhancement. If you experience
                      # OOM errors with your GPU, try reducing this.
-MODE=1   # Use which estimated output of the model: mode=1 is irm(conservative), mode=2 is lps(agressive), mode=3(fusion) is fusion of both.
-python main_denoising.py \
+MODE=3   #Use which output of the model: mode=1 is irm, mode=2 is lps, mode=3 is fusion of both.
+MODEL_SELECT=1000h  # Use which pre-trained model, currently including: a 400-h model and a 1000h-model.
+STAGE_SELECT=3 # Only works if choosing  1000h-model.
+
+/home/leisun1/anaconda3/envs/cntk-py35/bin/python /export/fs01/jsalt19/leisun/speech_enhancement/denoising_DIHARD18/main_denoising.py  \
        --verbose \
        --wav_dir $WAV_DIR --output_dir $SE_WAV_DIR \
        --use_gpu $USE_GPU --gpu_id $GPU_DEVICE_ID \
        --truncate_minutes $TRUNCATE_MINUTES \
-       --mode $MODE|| exit 1
+       --mode $MODE \
+	   --model_select $MODEL_SELECT \
+	   --stage_select $STAGE_SELECT 
 
-exit
+
+
+
+exit 0
+
+
+###################################
+# Perform VAD using enhanced audio
+###################################
+VAD_DIR=/data/vad  # Output directory for label files containing VAD output.
+HOPLENGTH=30  # Duration in milliseconds of frames for VAD. Also controls step size.
+MODE=3  # WebRTC aggressiveness. 0=least agressive and  3=most aggresive.
+NJOBS=1  # Number of parallel processes to use.
+python main_get_vad.py \
+       --verbose \
+       --wav_dir $SE_WAV_DIR --output_dir $VAD_DIR \
+       --mode $MODE --hoplength $HOPLENGTH \
+       --n_jobs $NJOBS || exit 1
+
+exit 0
